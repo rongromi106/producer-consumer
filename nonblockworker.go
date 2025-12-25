56d43bf512a9a89b5cf61b44f9e0b85185a4dbd3
Rongrong Miao <rongrong.miao@stackadapt.com>
2025-12-25 16:09:20 -0500

    Added implementation for non blocking producer

--- nonblockworker.go
+++ nonblockworker.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 )
+
+func nonblockproducer(ch chan<- int, start int, end int, id int) {
+	rand.Seed(time.Now().UnixNano())
+	for i := start; i <= end; i++ {
+		delay := time.Duration(rand.Intn(1000)) * time.Millisecond
+		fmt.Printf("producer %d sleeping %v before sending %d\n", id, delay, i)
+		time.Sleep(delay)
+		select {
+		case ch <- i:
+			fmt.Printf("Producer %d sent %d completed\n", id, i)
+		default:
+			// 	•	这个 i 永远不会被处理（没有重试), expect dropped message
+			fmt.Printf("producer failed to send %d\n", i)
+		}
+	}
+}
 
 func nonblockworker(ch <-chan int, id int) {
 	timer := time.NewTimer(500 * time.Millisecond)
@@ -40,8 +57,9 @@ func nonblockworker(ch <-chan int, id int) {
 }
 
 func NonBlockWorkerDemo() {
-	ch := make(chan int, 15)
+	ch := make(chan int, 5)
 	var wg sync.WaitGroup
+
 	workerCount := 4
 	wg.Add(workerCount)
 	for i := 0; i < workerCount; i++ {
@@ -51,6 +69,14 @@ func NonBlockWorkerDemo() {
 			nonblockworker(ch, id)
 		}()
 	}
-	go producer(ch, 1, 10, 0, true)
+	var wgP sync.WaitGroup
+	wgP.Add(1)
+	go func() {
+		defer wgP.Done()
+		nonblockproducer(ch, 1, 10, 0)
+	}()
+	wgP.Wait()
+	close(ch)
+
 	wg.Wait()
 }
